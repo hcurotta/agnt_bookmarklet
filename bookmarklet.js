28f1551f5ec216ab640f12ab61dc75c67a9f9f04
Harry Curotta <hcurotta@gmail.com>
2017-04-14 16:53:23 +0100

    fist

---
+++ bookmarklet.js
@@ -0,0 +1,32 @@
+(function(){
+  // the minimum version of jQuery we want
+  var v = "1.3.2";
+
+  // check prior inclusion and version
+  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+    var done = false;
+    var script = document.createElement("script");
+    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+    script.onload = script.onreadystatechange = function(){
+      if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+        done = true;
+        (function($){$.fn.replaceText=function(b,a,c){return this.each(function(){var f=this.firstChild,g,e,d=[];if(f){do{if(f.nodeType===3){g=f.nodeValue;e=g.replace(b,a);if(e!==g){if(!c&&/</.test(e)){$(f).before(e);d.push(f)}else{f.nodeValue=e}}}}while(f=f.nextSibling)}d.length&&$(d).remove()})}})(jQuery);
+        initBookmarklet();
+      }
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+  } else {
+    (function($){$.fn.replaceText=function(b,a,c){return this.each(function(){var f=this.firstChild,g,e,d=[];if(f){do{if(f.nodeType===3){g=f.nodeValue;e=g.replace(b,a);if(e!==g){if(!c&&/</.test(e)){$(f).before(e);d.push(f)}else{f.nodeValue=e}}}}while(f=f.nextSibling)}d.length&&$(d).remove()})}})(jQuery);
+    initBookmarklet();
+  }
+  
+  function initBookmarklet() {
+    (window.Bookmarklet = function() {
+
+      
+      debugger
+
+    })(); 
+  }
+
+})();
